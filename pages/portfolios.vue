906b308316a40ff1518e14007a3b81e1370c920f
otontas11 <oktay1125@gmail.com>
2025-10-11 09:28:38 +0300

    ver-3

--- pages/portfolios.vue
+++ pages/portfolios.vue
@@ -37,7 +37,7 @@
                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                 </svg>
-                Kullanıcı İlanları
+                Kullanıcı Portföyleri
               </button>
             </div>
           </div>
@@ -136,7 +136,7 @@
                   <svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                   </svg>
-                  Kullanıcı İlanı
+                  Kullanıcı Portföyü
                 </span>
               </div>
               
